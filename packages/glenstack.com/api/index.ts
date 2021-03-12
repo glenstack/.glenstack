f748e32dd378ba6dd9f8e5dcdf048560720c0a69
Greg Brimble <developer@gregbrimble.com>
2021-03-12 17:30:20 +0000

    A blank response for incoming webhooks

--- packages/glenstack.com/api/index.ts
+++ packages/glenstack.com/api/index.ts
@@ -1,8 +1,10 @@
 import { FABRuntime } from "@fab/core";
 import { registerEmail } from "./registerEmail";
+import { waitlistIncomingWebhook } from "./waitlistIncomingWebhook";
 
 const index = ({ Router }: FABRuntime) => {
   Router.on("/api/registerEmail", registerEmail);
+  Router.on("/api/waitlistIncomingWebhook", waitlistIncomingWebhook);
 };
 
 export default index;
