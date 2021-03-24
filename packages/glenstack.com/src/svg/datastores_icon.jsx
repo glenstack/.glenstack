a8c5777cc980d4306457ffb6a93bccf0d4eca0f3
James O'Donnell <james.o'donnell@skyscanner.net>
2021-03-24 21:16:58 +0000

    Add copy + icons

---
+++ packages/glenstack.com/src/svg/datastores_icon.jsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+
+function DatastoresIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={450}
+      height={500}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <ellipse cx={400} cy={489.792} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M380 444.457h40v45.2h-40z" />
+      <ellipse cx={400} cy={444.552} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={400} cy={443.083} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M380 397.75h40v45.2h-40z" />
+      <ellipse cx={400} cy={397.843} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={400} cy={396.374} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M380 351.041h40v45.2h-40z" />
+      <ellipse cx={400} cy={351.134} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={489.792} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 444.457h40v45.2H30z" />
+      <ellipse cx={50} cy={444.552} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={443.083} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 397.75h40v45.2H30z" />
+      <ellipse cx={50} cy={397.843} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={396.374} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 351.041h40v45.2H30z" />
+      <ellipse cx={50} cy={351.134} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={400} cy={392.792} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M380 347.457h40v45.2h-40z" />
+      <ellipse cx={400} cy={347.552} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={400} cy={346.083} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M380 300.75h40v45.2h-40z" />
+      <ellipse cx={400} cy={300.843} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={400} cy={299.374} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M380 254.041h40v45.2h-40z" />
+      <ellipse cx={400} cy={254.134} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={225} cy={489.792} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M205 444.457h40v45.2h-40z" />
+      <ellipse cx={225} cy={444.552} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={225} cy={443.083} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M205 397.75h40v45.2h-40z" />
+      <ellipse cx={225} cy={397.843} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={225} cy={396.374} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M205 351.041h40v45.2h-40z" />
+      <ellipse cx={225} cy={351.134} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={396.792} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 351.457h40v45.2H30z" />
+      <ellipse cx={50} cy={351.552} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={350.083} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 304.75h40v45.2H30z" />
+      <ellipse cx={50} cy={304.843} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={303.374} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 258.041h40v45.2H30z" />
+      <ellipse cx={50} cy={258.134} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={255.792} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 210.457h40v45.2H30z" />
+      <ellipse cx={50} cy={210.552} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={209.083} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 163.75h40v45.2H30z" />
+      <ellipse cx={50} cy={163.843} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={50} cy={162.374} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M30 117.041h40v45.2H30z" />
+      <ellipse cx={50} cy={117.134} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={225} cy={348.792} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M205 303.457h40v45.2h-40z" />
+      <ellipse cx={225} cy={303.552} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={225} cy={302.083} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M205 256.75h40v45.2h-40z" />
+      <ellipse cx={225} cy={256.843} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={225} cy={255.374} rx={20} ry={10.134} fill="#4E5B69" />
+      <path fill="#4E5B69" d="M205 210.041h40v45.2h-40z" />
+      <ellipse cx={225} cy={210.134} rx={20} ry={10.134} fill="#CBCBCB" />
+      <ellipse cx={225} cy={213.798} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M175 179.402h100v34.564H175z" />
+      <ellipse cx={225} cy={179.234} rx={50} ry={25.336} fill="#FFB800" />
+      <ellipse cx={225} cy={171.517} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M175 137.121h100v34.564H175z" />
+      <ellipse cx={225} cy={136.953} rx={50} ry={25.336} fill="#FFB800" />
+      <ellipse cx={225} cy={128.9} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M175 94.504h100v34.564H175z" />
+      <ellipse cx={225} cy={94.336} rx={50} ry={25.336} fill="#FFB800" />
+      <ellipse cx={400} cy={263.798} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M350 229.402h100v34.564H350z" />
+      <ellipse cx={400} cy={229.234} rx={50} ry={25.336} fill="#FFB800" />
+      <ellipse cx={400} cy={221.517} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M350 187.121h100v34.564H350z" />
+      <ellipse cx={400} cy={186.953} rx={50} ry={25.336} fill="#FFB800" />
+      <ellipse cx={400} cy={178.9} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M350 144.504h100v34.564H350z" />
+      <ellipse cx={400} cy={144.336} rx={50} ry={25.336} fill="#FFB800" />
+      <ellipse cx={50} cy={144.798} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M0 110.402h100v34.564H0z" />
+      <ellipse cx={50} cy={110.234} rx={50} ry={25.336} fill="#FFB800" />
+      <ellipse cx={50} cy={102.517} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M0 68.121h100v34.564H0z" />
+      <ellipse cx={50} cy={67.953} rx={50} ry={25.336} fill="#FFB800" />
+      <ellipse cx={50} cy={59.9} rx={50} ry={25.336} fill="#5F758E" />
+      <path fill="#5F758E" d="M0 25.504h100v34.564H0z" />
+      <ellipse cx={50} cy={25.336} rx={50} ry={25.336} fill="#FFB800" />
+    </svg>
+  );
+}
+
+export default DatastoresIcon;
--- packages/glenstack.com/tailwind.config.js
+++ packages/glenstack.com/tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       margin: {
         '100': '25rem',
         '120': '30rem',
+        '-120': '-30rem',        
         '128': '32rem',
         '136': '34rem',
         '152': '38rem',
@@ -23,18 +24,37 @@ module.exports = {
         '232': '58rem',
         '280': '70rem',
       },
+      height: {
+        '100': '25rem',
+        '120': '30rem',
+        '128': '32rem',
+        '136': '34rem',
+        '152': '38rem',
+        '156': '39rem',
+        '176': '44rem',
+        '192': '48rem',
+        '200': '50rem',       
+        '232': '58rem',
+        '280': '70rem',  
+        '300': '75rem',
+        '316': '79rem',  
+        '352': '88rem', 
+        '448': '112rem',                    
+      },      
       scale: {
+        '50': '.5',
+        '67': '.67',
         '25': '.25',
         '-1': '-1',
       },
       keyframes: {
         slideinleft: {
-          '0%': { transform: 'translateX(-750px)' },
-          '100%': { transform: 'translateX(0px)' }
+          '0%': { transform: 'translateX(-750px)', opacity: '0' },
+          '100%': { transform: 'translateX(0px)', opacity: '100' }
         },
         slideinright: {
-          '0%': { transform: 'translateX(1500px)' },
-          '100%': { transform: 'translateX(0px)' }
+          '0%': { transform: 'translateX(1500px)', opacity: '0' },
+          '100%': { transform: 'translateX(0px)', opacity: '100' }
         },  
         fadein: {
           '0%': { opacity: '0' },
@@ -48,7 +68,7 @@ module.exports = {
       animation: {
         fadein: 'fadein 1s ease-in-out',
         slideinright: 'slideinright 1s ease-in-out',         
-        slideinleft: 'slideinleft 2s ease-in-out',
+        slideinleft: 'slideinleft 1.5s ease-in-out',
         wiggle: 'wiggle 2s ease-in-out infinite',
       },
     },
