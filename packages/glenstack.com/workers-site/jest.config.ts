4bfa2ded2ad92a2b64b099fe3b880bfd67b41c11
Greg Brimble <developer@gregbrimble.com>
2021-05-08 13:41:24 +0100

    Fix coverages

--- packages/glenstack.com/workers-site/jest.config.ts
+++ packages/glenstack.com/workers-site/jest.config.ts
@@ -3,9 +3,9 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
-  displayName: "authentication",
+  displayName: "glenstack-com-worker",
   collectCoverageFrom: ["src/**/*"],
-  coverageReporters: [["lcov", { projectRoot: "../.." }]],
+  coverageReporters: [["lcov", { projectRoot: "../../.." }]],
 };
 
 export default config;
