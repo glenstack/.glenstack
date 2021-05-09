e2a7d859facd88df79a428e463e8bd9569d44750
Ragnor Comerford <hello@ragnor.co>
2021-05-09 16:04:17 +0100

    Feature: Add mutations

--- packages/api/src/graphql/schema/index.ts
+++ packages/api/src/graphql/schema/index.ts
@@ -1,8 +1,10 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { Expr, query as q } from 'faunadb';
 import { GraphQLFaunaObjectType, generateFaunaQuery } from "../../data/fauna";
-
+import _ from "lodash";
 import { client } from '../../data/fauna/client';
+import Debug from "debug";
+const debug = Debug("GraphQL");
 
 import {
 
@@ -16,9 +18,10 @@ import {
   GraphQLSchema,
   GraphQLString,
   GraphQLObjectType,
-  GraphQLID
+  GraphQLID,
+  GraphQLInputObjectType,
+  GraphQLNonNull
 } from "graphql"
-
 
 
 const bookType = new GraphQLFaunaObjectType({
@@ -30,8 +33,64 @@ const bookType = new GraphQLFaunaObjectType({
     authors: {type: GraphQLList(authorType)}
 
   }),
-  metaSchema:  {title: { fieldId: "294845251673129473", type:"string"}, authors: { fieldId: "294845329476420097", type:"relation",  from: 'A'}}
+  metaSchema:  {title: { fieldId: "294845251673129473", type:"string"}, authors: { fieldId: "294845329476420097", relationshipRef: q.Ref(q.Collection("relationships"), "296152190589862405"), type:"relation",  relation: "A"}}
 })
+
+class GraphQLFaunaInputObjectType extends GraphQLInputObjectType {
+  collectionName: string
+  constructor(args: any) {
+    super(args)
+    this.collectionName = args.collectionName
+    }
+}
+class GraphQLFaunaRelatedInputObjectType extends GraphQLInputObjectType {
+  relatedType: GraphQLFaunaInputObjectType
+  constructor(args: any) {
+    super(args)
+    this.relatedType = args.relatedType
+    }
+}
+const bookInputType = new GraphQLFaunaInputObjectType({
+  name: 'bookInput',
+  fields: () => ({
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    authors: {type: relatedAuthorsInputType}
+
+  }),
+})
+const authorInputType = new GraphQLFaunaInputObjectType({
+  name: 'authorInput',
+  fields: () => ({
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    books: {type: relatedBooksInputType}
+
+  }),
+})
+
+const relatedAuthorsInputType =  new GraphQLFaunaRelatedInputObjectType({
+  name: 'relatedAuthorsInput',
+  relatedType: authorInputType,
+  fields: () => ({
+    connect: { type: new GraphQLList(GraphQLID) },
+  })
+})
+const relatedBooksInputType =  new GraphQLFaunaRelatedInputObjectType({
+  name: 'relatedBooksInput',
+  relatedType: bookInputType,
+  fields: () => ({
+    connect: { type: new GraphQLList(GraphQLID) },
+  })
+})
+
+
+// const relatedInputType = new GraphQLInputObjectType({
+//   name: 'relatedInput',
+//   fields: () => ({
+
+//     ids
+
+
+//   })})
 
 const authorType = new GraphQLFaunaObjectType({
   name: 'Author',
@@ -42,7 +101,7 @@ const authorType = new GraphQLFaunaObjectType({
     books: {type: GraphQLList(bookType)}
     
   }),
-  metaSchema:  {name: { fieldId: "294845354656924161", type:"string"}, books: { fieldId: "294845383336526337", type:"relation",  from: 'B'}}
+  metaSchema:  {name: { fieldId: "294845354656924161", type:"string"}, books: { fieldId: "294845383336526337",relationshipRef: q.Ref(q.Collection("relationships"), "296152190589862405"), type:"relation",  relation: "B"}}
 })
 
 const queryType = new GraphQLObjectType({
@@ -87,9 +146,44 @@ const queryType = new GraphQLObjectType({
   }
 });
 
+const mutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    createBook: {
+      type: GraphQLList(bookType),
+      args: {
+        input: { type: bookInputType }
+    },
+    resolve: async (obj: any, args, context: any, info: any) => {
+      debug(args)
+      console.log(JSON.stringify(args)) 
+      console.log(info)
+      let data = {}
+      let relationQueries
+      for (let [key, value] of Object.entries(args.input)) {
+        if (bookType.metaSchema[key].type === "relation") {
+          console.log("side:"+bookType.metaSchema[key].relation)
+          relationQueries = q.Create(q.Collection("relations"),{data: {
+            relationshipRef: bookType.metaSchema[key].relationshipRef,
+            [bookType.metaSchema[key].relation]:  q.Var("docRef"),
+            [bookType.metaSchema[key].relation === "A" ? "B" : "A"] : q.Ref(q.Collection("294845159814726145"),value.connect[0])
+          }})
+        }
+        else {
+          data[bookType.metaSchema[key].fieldId] = value
+        }
+      }
+
+      // let data = {data: Object.fromEntries(Object.entries(args.input).filter(([key, value]) => bookType.metaSchema[key].type !== "relation").map( ([key, value]) => [bookType.metaSchema[key].fieldId, value] ))}
+      let res = await client.query(q.Let({docRef: q.Select(['ref'],q.Create(q.Collection(bookType.collectionName),{data}))},{finished: relationQueries}))
+      console.log(JSON.stringify(data))
+      console.log(JSON.stringify(res))
+                            return {}
+  }
+}}})
 
 
-export const schema  = new GraphQLSchema({query: queryType});
+export const schema  = new GraphQLSchema({query: queryType, mutation: mutationType});
 // export const schema = makeExecutableSchema({ typeDefs, resolvers,  schemaDirectives: {
 //   renameField
 // } });
