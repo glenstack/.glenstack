9be5fb4e4306a7dfb29a5c342132e6696903ae16
Greg Brimble <developer@gregbrimble.com>
2021-05-18 19:16:16 +0100

    .

--- packages/glenstack.com/src/components/landing/features.tsx
+++ packages/glenstack.com/src/components/landing/features.tsx
@@ -67,7 +67,7 @@ export const Features: FC = () => {
     <div className="bg-indigo-900">
       <div className="max-w-4xl mx-auto px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24 lg:px-8">
         <h2 className="text-3xl font-extrabold text-white tracking-tight">
-          The Platform for Data
+          The Platform for Data.
         </h2>
         <div className="flex">
           <div className="flex-initial">
