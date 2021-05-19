9f361d4827b565d7ba009b21b1b0d6e278b359c3
Greg Brimble <developer@gregbrimble.com>
2021-05-19 13:48:27 +0100

    Fix Twitter

--- packages/glenstack.com/src/pages/authentication/waitlist.tsx
+++ packages/glenstack.com/src/pages/authentication/waitlist.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { Footer } from "../../components/landing/footer";
 import { Header } from "../../components/landing/header";
 import { SEO } from "../../components/landing/seo";
 
@@ -10,6 +11,12 @@ export const WaitList: FC = () => {
         description="Join the Glenstack Waitlist before we launch in Summer, 2021."
       />
       <Header />
+      <main>
+        <form method="POST" action="https://auth.glenstack.com/waitlist">
+          <input type="email"></input>
+        </form>
+      </main>
+      <Footer />
     </>
   );
 };
