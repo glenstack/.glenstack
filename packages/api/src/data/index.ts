5a051d1e6ec3005cb3122ed60188b2ca8c84295a
Ragnor Comerford <hello@ragnor.co>
2021-05-29 19:40:55 +0100

    Add error handling

--- packages/api/src/data/index.ts
+++ packages/api/src/data/index.ts
@@ -11,112 +11,119 @@ import {
   ScalarFieldInput,
   TableInput,
 } from "./types";
+import to from "await-to-js";
 
 export const getSchema = async (): Promise<GraphQLSchema> => {
-  const data = await client.query<{
-    organizations: Array<{ projects: Array<unknown> }>;
-  }>({
-    organizations: q.Select(
-      "data",
-      q.Map(
-        q.Paginate(q.Documents(q.Collection("organizations"))),
-        q.Lambda(
-          "organizationRef",
-          q.Merge(
-            q.Select("data", q.Get(q.Var("organizationRef"))),
+  const [err, data] = await to(
+    client.query<{
+      organizations: Array<{ projects: Array<unknown> }>;
+    }>({
+      organizations: q.Select(
+        "data",
+        q.Map(
+          q.Paginate(q.Documents(q.Collection("organizations"))),
+          q.Lambda(
+            "organizationRef",
+            q.Merge(
+              q.Select("data", q.Get(q.Var("organizationRef"))),
 
-            {
-              projects: q.Select(
-                "data",
-                q.Map(
-                  q.Paginate(
-                    q.Match(
-                      q.Index("projects_by_organization"),
-                      q.Var("organizationRef")
-                    )
-                  ),
-                  q.Lambda(
-                    "projectRef",
-                    q.Merge(q.Select("data", q.Get(q.Var("projectRef"))), {
-                      tables: q.Select(
-                        "data",
-                        q.Map(
-                          q.Paginate(
-                            q.Match(
-                              q.Index("tables_by_project"),
-                              q.Var("projectRef")
-                            )
-                          ),
-                          q.Lambda(
-                            "tableRef",
-                            q.Merge(
-                              q.Select("data", q.Get(q.Var("tableRef"))),
-                              {
-                                id: q.Select(
-                                  ["ref", "id"],
-                                  q.Get(q.Var("tableRef"))
-                                ),
-                                fields: q.Select(
-                                  "data",
-                                  q.Map(
-                                    q.Paginate(
-                                      q.Match(
-                                        q.Index("fields_by_table"),
-                                        q.Var("tableRef")
-                                      )
-                                    ),
-                                    q.Lambda(
-                                      "fieldRef",
-                                      q.Merge(
-                                        q.Select(
-                                          "data",
-                                          q.Get(q.Var("fieldRef"))
-                                        ),
-                                        [
-                                          {
-                                            id: q.Select(
-                                              ["ref", "id"],
-                                              q.Get(q.Var("fieldRef"))
-                                            ),
-                                          },
-                                          q.If(
-                                            q.ContainsPath(
-                                              ["data", "relationshipRef"],
-                                              q.Get(q.Var("fieldRef"))
-                                            ),
+              {
+                projects: q.Select(
+                  "data",
+                  q.Map(
+                    q.Paginate(
+                      q.Match(
+                        q.Index("projects_by_organization"),
+                        q.Var("organizationRef")
+                      )
+                    ),
+                    q.Lambda(
+                      "projectRef",
+                      q.Merge(q.Select("data", q.Get(q.Var("projectRef"))), {
+                        tables: q.Select(
+                          "data",
+                          q.Map(
+                            q.Paginate(
+                              q.Match(
+                                q.Index("tables_by_project"),
+                                q.Var("projectRef")
+                              )
+                            ),
+                            q.Lambda(
+                              "tableRef",
+                              q.Merge(
+                                q.Select("data", q.Get(q.Var("tableRef"))),
+                                {
+                                  id: q.Select(
+                                    ["ref", "id"],
+                                    q.Get(q.Var("tableRef"))
+                                  ),
+                                  fields: q.Select(
+                                    "data",
+                                    q.Map(
+                                      q.Paginate(
+                                        q.Match(
+                                          q.Index("fields_by_table"),
+                                          q.Var("tableRef")
+                                        )
+                                      ),
+                                      q.Lambda(
+                                        "fieldRef",
+                                        q.Merge(
+                                          q.Select(
+                                            "data",
+                                            q.Get(q.Var("fieldRef"))
+                                          ),
+                                          [
                                             {
-                                              relationship: q.Select(
-                                                ["data"],
-                                                q.Get(
-                                                  q.Select(
-                                                    ["data", "relationshipRef"],
-                                                    q.Get(q.Var("fieldRef"))
-                                                  )
-                                                )
+                                              id: q.Select(
+                                                ["ref", "id"],
+                                                q.Get(q.Var("fieldRef"))
                                               ),
                                             },
-                                            {}
-                                          ),
-                                        ]
+                                            q.If(
+                                              q.ContainsPath(
+                                                ["data", "relationshipRef"],
+                                                q.Get(q.Var("fieldRef"))
+                                              ),
+                                              {
+                                                relationship: q.Select(
+                                                  ["data"],
+                                                  q.Get(
+                                                    q.Select(
+                                                      [
+                                                        "data",
+                                                        "relationshipRef",
+                                                      ],
+                                                      q.Get(q.Var("fieldRef"))
+                                                    )
+                                                  )
+                                                ),
+                                              },
+                                              {}
+                                            ),
+                                          ]
+                                        )
                                       )
                                     )
-                                  )
-                                ),
-                              }
+                                  ),
+                                }
+                              )
                             )
                           )
-                        )
-                      ),
-                    })
+                        ),
+                      })
+                    )
                   )
-                )
-              ),
-            }
+                ),
+              }
+            )
           )
         )
-      )
-    ),
-  });
+      ),
+    })
+  );
+  if (!data) throw new Error("Failed to fetch schema from Fauna: " + err);
   return generateGraphQLSchema(data.organizations[0].projects[0]);
 };
 
@@ -125,7 +132,7 @@ export const createOrganization = async ({
 }: {
   name: string;
 }): Promise<unknown> => {
-  const organizationPayload: OrganizationInput = { name, apiName: name };
+  const organizationInput: OrganizationInput = { name, apiName: name };
 
   const {
     data,
@@ -133,7 +140,7 @@ export const createOrganization = async ({
   } = await client.query<FaunaResponse>(
     q.Create(q.Collection("organizations"), {
       data: {
-        ...organizationPayload,
+        ...organizationInput,
       },
     })
   );
