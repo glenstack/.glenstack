e857c52e4371efaa9b71f2a3842be78357167a07
Ragnor Comerford <ragnor.comerford@gmail.com>
2021-06-05 18:31:10 +0000

    Add project schema getter

--- packages/api/src/data/index.ts
+++ packages/api/src/data/index.ts
@@ -4,7 +4,75 @@ import generateGraphQLSchema from "./generateGraphQLSchema";
 import { GraphQLSchema } from "graphql";
 import to from "await-to-js";
 
-export const getSchema = async (client: Client): Promise<GraphQLSchema> => {
+export const getProjectSchema = async (
+  client: Client,
+  projectId: string
+): Promise<GraphQLSchema> => {
+  const [err, data] = await to(
+    client.query(
+      q.Let(
+        { projectRef: q.Ref(q.Collection("projects"), projectId) },
+        q.Merge(q.Select("data", q.Get(q.Var("projectRef"))), {
+          tables: q.Select(
+            "data",
+            q.Map(
+              q.Paginate(
+                q.Match(q.Index("tables_by_project"), q.Var("projectRef"))
+              ),
+              q.Lambda(
+                "tableRef",
+                q.Merge(q.Select("data", q.Get(q.Var("tableRef"))), {
+                  id: q.Select(["ref", "id"], q.Get(q.Var("tableRef"))),
+                  fields: q.Select(
+                    "data",
+                    q.Map(
+                      q.Paginate(
+                        q.Match(q.Index("fields_by_table"), q.Var("tableRef"))
+                      ),
+                      q.Lambda(
+                        "fieldRef",
+                        q.Merge(q.Select("data", q.Get(q.Var("fieldRef"))), [
+                          {
+                            id: q.Select(
+                              ["ref", "id"],
+                              q.Get(q.Var("fieldRef"))
+                            ),
+                          },
+                          q.If(
+                            q.ContainsPath(
+                              ["data", "relationshipRef"],
+                              q.Get(q.Var("fieldRef"))
+                            ),
+                            {
+                              relationship: q.Select(
+                                ["data"],
+                                q.Get(
+                                  q.Select(
+                                    ["data", "relationshipRef"],
+                                    q.Get(q.Var("fieldRef"))
+                                  )
+                                )
+                              ),
+                            },
+                            {}
+                          ),
+                        ])
+                      )
+                    )
+                  ),
+                })
+              )
+            )
+          ),
+        })
+      )
+    )
+  );
+  if (!data) throw new Error("Failed to fetch schema from Fauna: " + err);
+  return generateGraphQLSchema(data, client);
+};
+
+export const getUserSchema = async (client: Client): Promise<GraphQLSchema> => {
   const [err, data] = await to(
     client.query<{
       organizations: Array<{ projects: Array<unknown> }>;
