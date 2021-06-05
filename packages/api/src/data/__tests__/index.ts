1c8fe1956cbe14a6ee8e3c16289c223d154c647b
Ragnor Comerford <ragnor.comerford@gmail.com>
2021-06-05 19:10:31 +0000

    Fix tests.

--- packages/api/src/data/__tests__/index.ts
+++ packages/api/src/data/__tests__/index.ts
@@ -1,108 +1,102 @@
-// import { ApolloServer } from "apollo-server";
-// import { Client, query as q } from "faunadb";
-// import { getSchema } from "../index";
-// import { GraphQLSchema } from "graphql";
-// import scaffold from "../fauna/scaffold";
-// import admin from "../fauna/admin";
-// import { definitions } from "../definitions";
-// import { Table } from "../types";
+import { ApolloServer } from "apollo-server";
+import { Client, query as q } from "faunadb";
+import { getProjectSchema } from "../index";
+import { GraphQLSchema } from "graphql";
+import scaffold from "../fauna/scaffold";
+import admin from "../fauna/admin";
+import { definitions } from "../definitions";
+import { Table } from "../types";
 
-// jest.setTimeout(30000);
+jest.setTimeout(30000);
+test("Initial query should be empty", async () => {
+  const test_db_name = "cnewtest_" + Date.now();
 
-// const test_db_name = "cnewtest_" + Date.now();
+  const client = new Client({
+    secret: "fnAEKpUbg1ACBTYHxtqayKNrCnnmgHLyWoSSlyvX",
+  });
 
-// const client = new Client({
-//   secret: "fnAEKpUbg1ACBTYHxtqayKNrCnnmgHLyWoSSlyvX",
-// });
-// let test_client: Client;
+  const tables: Record<string, Omit<Table, "fields">> = {};
 
-// let server: ApolloServer;
+  await client.query(q.CreateDatabase({ name: test_db_name }));
 
-// let tables: Record<string, Omit<Table, "fields">>;
+  const { secret }: { secret: string } = await client.query(
+    q.CreateKey({
+      database: q.Database(test_db_name),
+      role: "server",
+    })
+  );
+  const test_client = new Client({
+    secret,
+  });
+  const {
+    createOrganization,
+    createProject,
+    createTable,
+    createScalarField,
+    createRelationshipField,
+  } = admin(test_client);
+  await scaffold(test_client);
+  const { id: organizationId } = await createOrganization({
+    name: "LibraryOrg",
+  });
+  const { id: projectId } = await createProject({
+    name: "LibraryProj",
+    organizationId,
+  });
 
-// beforeAll(async () => {
-//   await client.query(q.CreateDatabase({ name: test_db_name }));
+  tables["Books"] = await createTable({
+    name: "Books",
+    projectId,
+  });
+  tables["Authors"] = await createTable({
+    name: "Authors",
+    projectId,
+  });
 
-//   const { secret }: { secret: string } = await client.query(
-//     q.CreateKey({
-//       database: q.Database(test_db_name),
-//       role: "server",
-//     })
-//   );
-//   test_client = new Client({
-//     secret,
-//   });
-//   const {
-//     createOrganization,
-//     createProject,
-//     createTable,
-//     createScalarField,
-//     createRelationshipField,
-//   } = admin(test_client);
-//   await scaffold(test_client);
-//   const { id: organizationId } = await createOrganization({
-//     name: "LibraryOrg",
-//   });
-//   const { id: projectId } = await createProject({
-//     name: "LibraryProj",
-//     organizationId,
-//   });
+  await createScalarField({
+    name: "title",
+    tableId: tables["Books"].id,
+    type: "String",
+  });
+  await createScalarField({
+    name: "name",
+    tableId: tables["Authors"].id,
+    type: "String",
+  });
 
-//   tables["Books"] = await createTable({
-//     name: "Books",
-//     projectId,
-//   });
-//   tables["Authors"] = await createTable({
-//     name: "Authors",
-//     projectId,
-//   });
+  await createRelationshipField({
+    name: "authors",
+    backName: "books",
+    to: tables["Authors"].id,
+    tableId: tables["Books"].id,
+  });
 
-//   await createScalarField({
-//     name: "title",
-//     tableId: tables["Books"].id,
-//     type: "String",
-//   });
-//   await createScalarField({
-//     name: "name",
-//     tableId: tables["Authors"].id,
-//     type: "String",
-//   });
+  const schema: GraphQLSchema = await getProjectSchema(test_client, projectId);
+  const server = new ApolloServer({
+    schema,
+  });
 
-//   await createRelationshipField({
-//     name: "authors",
-//     backName: "books",
-//     to: tables["Authors"].id,
-//     tableId: tables["Books"].id,
-//   });
+  console.log(`query { ${definitions(tables["Books"]).queries.findMany.name()} {
+      title
+      authors {
+        name
+      }
+    }}`);
+  const queryName = definitions(tables["Books"]).queries.findMany.name();
+  const result = await server.executeOperation({
+    query: `query { ${queryName} {
+        title
 
-//   const schema: GraphQLSchema = await getSchema(test_client);
-//   server = new ApolloServer({
-//     schema,
-//   });
-// });
+      }}`,
+    variables: {},
+  });
+  expect(result.errors).toBeUndefined();
+  expect((result.data || {})[queryName]).toEqual([]);
+  console.log(await client.query(q.Collection(tables["Books"].id)));
+  await client.query(q.Delete(q.Database(test_db_name)));
 
-// test("Initial query should be empty", async () => {
-//   console.log(`query { ${definitions(tables["Books"]).queries.findMany.name()} {
-//       title
-//       authors {
-//         name
-//       }
-//     }}`);
-//   // const tables =
-//   const result = await server.executeOperation({
-//     query: `query { ${definitions(tables["Books"]).queries.findMany.name()} {
-//         title
-
-//       }}`,
-//     variables: {},
-//   });
-//   expect(result.errors).toBeUndefined();
-//   expect(result.data).toBe([]);
-//   console.log(await client.query(q.Collection(tables["Books"].id)));
-//   // await client.query(q.Delete(q.Database(test_db_name)));
-
-//   expect(2 + 2).toBe(4);
-// });
+  expect(2 + 2).toBe(4);
+});
 test("fake", () => {
   expect(2 + 2).toBe(4);
 });
